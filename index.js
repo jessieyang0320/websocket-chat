5cb58deb78ce8f1c5d97a111a0dd1605a1978c39
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-08-30 22:23:33 -0400

    chat function works, needs improvements

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var ws = require("nodejs-websocket")
 var PORT = 3000
 
+var clientCount = 0
+
 // Scream server example: "hi" -> "HI!!!"
 var server = ws.createServer(function (conn) {
 	console.log("New connection")
+
+	clientCount++
+	conn.nickname = 'user' + clientCount
+	broadcast(conn.nickname + 'is here')
 	conn.on("text", function (str) {
 		console.log("Received "+str)
-		conn.sendText(str)
+		broadcast(str)
 	})
 	conn.on("close", function (code, reason) {
 		console.log("Connection closed")
+		broadcast(conn.nickname + 'left')
 	})
 
 	conn.on('error', function(err){
@@ -19,3 +26,9 @@ var server = ws.createServer(function (conn) {
 }).listen(PORT)
 
 console.log("websocket server listen on port" + PORT)
+
+function broadcast(str){
+	server.connections.forEach(function(connection){
+		connection.sendText(str)
+	})
+}
